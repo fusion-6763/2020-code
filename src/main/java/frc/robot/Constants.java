97c34c3649530670ed81185f473b94d70f010d50
Matt Coughlin <coughlin26@gmail.com>
2020-01-06 18:46:22 -0500

    Added drive constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,12 +8,30 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
+    public static final class DriveConstants {
+        public static final int leftMotorPort1 = 0;
+        public static final int leftMotorPort2 = 1;
+        public static final int rightMotorPort1 = 2;
+        public static final int rightMotorPort2 = 3;
+
+        public static final int[] leftEncoderPorts = { 0, 1 };
+        public static final int[] rightEncoderPorts = { 2, 3 };
+        public static final boolean leftEncoderReversed = false;
+        public static final boolean rightEncoderReversed = true;
+
+        public static final int encoderCountsPerRotation = 1024;
+        public static final double wheelDiameter = 6;
+        public static final double encoderDistancePerPulse = (wheelDiameter * Math.PI)
+                / (double) encoderCountsPerRotation;
+    }
 }
