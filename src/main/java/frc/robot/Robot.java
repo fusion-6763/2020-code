bf678f738ec2c159268056964892a649fc69c423
Caleb Denio <cjdenio44@gmail.com>
2020-01-08 18:15:56 -0500

    Add Teleop stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.Teleop;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -22,6 +23,8 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+
+  Teleop teleopCommand = new Teleop();
 
   /**
    * This function is run when the robot is first started up and should be used
@@ -104,6 +107,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
+    teleopCommand.schedule();
   }
 
   @Override
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -8,12 +8,25 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Spark;
+
+import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
+  public Spark leftMotor = new Spark(Constants.DriveConstants.leftMotorPort1);
+  public Spark rightMotor = new Spark(Constants.DriveConstants.rightMotorPort1);
+
+  DifferentialDrive myRobot = new DifferentialDrive(leftMotor, rightMotor);
+
   /**
    * Creates a new ExampleSubsystem.
    */
   public DriveTrain() {
+  }
+
+  public void drive(Double speed, Double rotationSpeed){
+    myRobot.arcadeDrive(speed, rotationSpeed);
   }
 
   @Override
