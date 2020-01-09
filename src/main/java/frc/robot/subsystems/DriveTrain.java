35af9b0f52d58e79d482a8100b88f5fbb8dec813
Matt Coughlin <coughlin26@gmail.com>
2020-01-08 19:32:40 -0500

    Updated a comment

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
   /**
-   * Creates a new ExampleSubsystem.
+   * Creates a new DriveTrain.
    */
   public DriveTrain() {
   }
