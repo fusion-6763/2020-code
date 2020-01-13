b959e61ffa64bcd2a0b2d6a687799b7ba0d614fd
Matt Coughlin <coughlin26@gmail.com>
2020-01-13 18:26:32 -0500

    Optimized imports

--- src/main/java/frc/robot/commands/Intake.java
+++ src/main/java/frc/robot/commands/Intake.java
@@ -1,12 +1,10 @@
 package frc.robot.commands;
 
-import frc.robot.subsystems.BallIntake;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj.Timer;
 
 public class Intake extends CommandBase {
 
   public void intake() {
-    
+
   }
 }
