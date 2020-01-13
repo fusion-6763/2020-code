60eb75bd455537808f15eb813ca4b2831505b4e4
Xavier P <44008221+Xavosorcerer@users.noreply.github.com>
2020-01-13 17:59:25 -0500

    Separated Ball_Intake from Shooter subsystem.

---
+++ src/main/java/frc/robot/subsystems/Ball_Intake.java
@@ -0,0 +1,27 @@
+//Operates the robot motors that control the ball-shooting mechanism. 
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.DriveConstants.SHOOTER_PORT;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Ball_Intake extends SubsystemBase {
+  // The spark that operates the motor.
+  private final Spark motor = new Spark(SHOOTER_PORT);
+
+  // Launches that ball at full speed forwards.
+  public void OutputBall() {
+    motor.set(1);
+  };
+
+  // Inputs the ball a full speed backwards.
+  public void InputBall() {
+    motor.set(-1);
+  };
+
+  // Stops spinning the motor.
+  public void Neutral() {
+    motor.set(0);
+  }
+}
