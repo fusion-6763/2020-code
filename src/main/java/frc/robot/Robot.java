9217f3686b3cdb16346b5060bdfcbc9d265308ca
Caleb Denio <cjdenio44@gmail.com>
2020-01-13 19:27:43 -0500

    Removed unused import

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,7 +10,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.BallIntake;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -33,8 +32,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our
     // autonomous chooser on the dashboard.
-    
-    //BallIntake.setDefaultCommand();
+
+    // BallIntake.setDefaultCommand();
   }
 
   /**
