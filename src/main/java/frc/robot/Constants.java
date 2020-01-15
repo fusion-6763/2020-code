36e22ded6c0d9fd6a5f5b568586a48bd05a2280f
Matt Coughlin <coughlin26@gmail.com>
2020-01-15 18:29:35 -0500

    Fixed constants indentation

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,39 +21,39 @@ import edu.wpi.first.wpilibj.util.ColorShim;
  * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public static final class DriveConstants {
-        public static final int LEFT_MOTOR_PORT = 0;
-        public static final int RIGHT_MOTOR_PORT = 2;
+  public static final class DriveConstants {
+    public static final int LEFT_MOTOR_PORT = 0;
+    public static final int RIGHT_MOTOR_PORT = 2;
 
-        public static final int[] LEFT_ENCODER_PORTS = { 0, 1 };
-        public static final int[] RIGHT_ENCODER_PORTS = { 2, 3 };
-        public static final boolean LEFT_ENCODER_REVERSED = false;
-        public static final boolean RIGHT_ENCODER_REVERSED = true;
+    public static final int[] LEFT_ENCODER_PORTS = { 0, 1 };
+    public static final int[] RIGHT_ENCODER_PORTS = { 2, 3 };
+    public static final boolean LEFT_ENCODER_REVERSED = false;
+    public static final boolean RIGHT_ENCODER_REVERSED = true;
 
-        public static final int ENCODER_COUNTS_PER_ROTATION = 1024;
-        public static final double WHEEL_DIAMETER = 6;
-        public static final double ENCODER_DISTANCE_PER_PULSE = (WHEEL_DIAMETER * Math.PI)
-                / (double) ENCODER_COUNTS_PER_ROTATION;
-    }
+    public static final int ENCODER_COUNTS_PER_ROTATION = 1024;
+    public static final double WHEEL_DIAMETER = 6;
+    public static final double ENCODER_DISTANCE_PER_PULSE = (WHEEL_DIAMETER * Math.PI)
+        / (double) ENCODER_COUNTS_PER_ROTATION;
+  }
 
-    public static final class IntakeConstants {
-        public static final int INTAKE_PORT1 = 3;
-        public static final int INTAKE_PORT2 = 4;
-    }
+  public static final class IntakeConstants {
+    public static final int INTAKE_PORT1 = 3;
+    public static final int INTAKE_PORT2 = 4;
+  }
 
-    public static final class ShooterConstants {
-        public static final int SHOOTER_PORT1 = 5;
-    }
+  public static final class ShooterConstants {
+    public static final int SHOOTER_PORT1 = 5;
+  }
 
-    public static final class ControllerConstants {
-        public static final int DRIVER_PORT = 0;
-        public static final int SHOOTER_PORT = 1;
-    }
+  public static final class ControllerConstants {
+    public static final int DRIVER_PORT = 0;
+    public static final int SHOOTER_PORT = 1;
+  }
 
-    public static final class WheelColors {
-        public static final Color BLUE = new ColorShim(0.143, 0.427, 0.429);
-        public static final Color GREEN = new ColorShim(0.197, 0.561, 0.240);
-        public static final Color RED = new ColorShim(0.561, 0.232, 0.114);
-        public static final Color YELLOW = new ColorShim(0.361, 0.524, 0.113);
-    }
+  public static final class WheelColors {
+    public static final Color BLUE = new ColorShim(0.143, 0.427, 0.429);
+    public static final Color GREEN = new ColorShim(0.197, 0.561, 0.240);
+    public static final Color RED = new ColorShim(0.561, 0.232, 0.114);
+    public static final Color YELLOW = new ColorShim(0.361, 0.524, 0.113);
+  }
 }
