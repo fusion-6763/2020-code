7f609fdebc3ddfee65c132a310d5537b94c69e92
Xavier P <44008221+Xavosorcerer@users.noreply.github.com>
2020-01-16 18:05:56 -0500

    Created camera class and port constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -43,6 +43,8 @@ public final class Constants {
 
   public static final class ShooterConstants {
     public static final int SHOOTER_PORT1 = 5;
+
+    public static final int[] CAMERA_PORTS = { 6, 7, 8 };
   }
 
   public static final class ControllerConstants {
---
+++ src/main/java/frc/robot/subsystems/TrackingCamera.java
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.ShooterConstants.CAMERA_PORTS;
+
+/**
+ * Add your docs here.
+ */
+public class TrackingCamera {
+  int port1 = CAMERA_PORTS[0];
+}
