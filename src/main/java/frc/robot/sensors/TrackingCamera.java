010ccc51743f8eb8cec99aa5375e49df66fde2c2
Matt Coughlin <coughlin26@gmail.com>
2020-01-18 13:46:45 -0500

    Started adding methods for the pixy

--- src/main/java/frc/robot/sensors/TrackingCamera.java
+++ src/main/java/frc/robot/sensors/TrackingCamera.java
@@ -7,14 +7,69 @@
 
 package frc.robot.sensors;
 
-import static frc.robot.Constants.CameraConstants.CAMERA_PORTS;
+import java.util.ArrayList;
+
+import io.github.pseudoresonance.pixy2api.Pixy2;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
+import io.github.pseudoresonance.pixy2api.links.Link;
+import io.github.pseudoresonance.pixy2api.links.SPILink;
 
 /**
- * This class defines the camera used for vision tracking.
+ * This class defines the Pixy camera functionality.
  */
 public class TrackingCamera {
-  private final int _port1 = CAMERA_PORTS[0];
+  private final Link _link = new SPILink();
+  private final Pixy2 _pixy = Pixy2.createInstance(_link);
+  private Block _biggestBlock = null;
 
   public TrackingCamera() {
+    _pixy.init();
+    _biggestBlock = getBiggestBlock();
+  }
+
+  public int getXCoordinate() {
+    return _biggestBlock.getX();
+  }
+
+  public int getYCoordinate() {
+    return _biggestBlock.getY();
+  }
+
+  public int getAngle() {
+    return _biggestBlock.getAngle();
+  }
+
+  public int getWidth() {
+    return _biggestBlock.getWidth();
+  }
+
+  public int getHeight() {
+    return _biggestBlock.getHeight();
+  }
+
+  /**
+   * This finds the largest target in the current frame. This is hidden
+   * intentionally, so the user is always referencing the same block.
+   * 
+   * @return The biggest target in the current frame.
+   */
+  private Block getBiggestBlock() {
+    final int blockCount = _pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 25);
+
+    if (blockCount <= 0) {
+      return null;
+    }
+
+    final ArrayList<Block> blocks = _pixy.getCCC().getBlocks();
+    Block largestBlock = null;
+    for (Block block : blocks) {
+      if (largestBlock == null) {
+        largestBlock = block;
+      } else if (block.getWidth() > largestBlock.getWidth()) {
+        largestBlock = block;
+      }
+    }
+    return largestBlock;
   }
 }
