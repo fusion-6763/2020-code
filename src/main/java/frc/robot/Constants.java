15aa160d356022528edd8cb6a23bff5b0759cf1f
Matt Coughlin <coughlin26@gmail.com>
2020-01-20 19:51:27 -0500

    Changed the shooter port

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -42,7 +42,7 @@ public final class Constants {
   }
 
   public static final class ShooterConstants {
-    public static final int SHOOTER_PORT1 = 5;
+    public static final int SHOOTER_PORT1 = 9;
 
   }
 
