79c8a2a334ef00bc2c64fd655701f42fee509de2
Caleb Denio <cjdenio44@gmail.com>
2020-01-25 10:32:12 -0500

    Added Limelight subsystem

---
+++ src/main/java/frc/robot/sensors/Limelight.java
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.sensors;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+/**
+ * Add your docs here.
+ */
+public class Limelight {
+  NetworkTable table;
+
+  public Limelight(String networkTableName) {
+    table = NetworkTableInstance.getDefault().getTable(networkTableName);
+  }
+
+  public double getX() {
+    return table.getEntry("tx").getDouble(0.0);
+  }
+
+  public double getY() {
+    return table.getEntry("ty").getDouble(0.0);
+  }
+}
