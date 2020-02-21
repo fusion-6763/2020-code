f96fb2a8f82910b13c269d8259c57666526636d7
Xavier P <44008221+Xavosorcerer@users.noreply.github.com>
2020-02-20 19:36:46 -0500

    Teleop controls! 🙌

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,16 +11,26 @@ import static frc.robot.Constants.CameraConstants.CHAMELEON_CAMERA_NAME;
 import static frc.robot.Constants.ControllerConstants.DRIVER_PORT;
 import static frc.robot.Constants.ControllerConstants.SHOOTER_PORT;
 
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+
+import java.awt.Button;
+
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.Aim;
 import frc.robot.commands.FindPowerCell;
 import frc.robot.commands.Intake;
 import frc.robot.commands.Shoot;
 import frc.robot.commands.Teleop;
+import frc.robot.commands.RunHopper;
+import frc.robot.commands.LoadBall;
+import frc.robot.commands.RunArm;
+import frc.robot.commands.RunArm.ArmMode;
 import frc.robot.commands.automodes.JustDriveAuto;
 import frc.robot.commands.automodes.SideAuto;
 import frc.robot.commands.automodes.SimpleAuto;
@@ -43,12 +53,15 @@ import frc.robot.subsystems.Turret;
  */
 public class RobotContainer {
   private final XboxController _driverController = new XboxController(DRIVER_PORT);
-  private final Joystick _shooterController = new Joystick(SHOOTER_PORT);
-  private final JoystickButton _leftShooterBumper = new JoystickButton(_shooterController,
-      XboxController.Button.kBumperLeft.value);
-  private final JoystickButton _rightShooterBumper = new JoystickButton(_shooterController,
-      XboxController.Button.kBumperRight.value);
   private final JoystickButton _xButton = new JoystickButton(_driverController, XboxController.Button.kX.value);
+
+  private final Joystick _shooterController = new Joystick(SHOOTER_PORT);
+  private final JoystickButton _trigger = new JoystickButton(_shooterController, 1);
+  private final JoystickButton _thumbButton = new JoystickButton(_shooterController, 2);
+  private final JoystickButton _topButton0 = new JoystickButton(_shooterController, 3);
+  private final JoystickButton _topButton1 = new JoystickButton(_shooterController, 4);
+  private final JoystickButton _topButton2 = new JoystickButton(_shooterController, 5);
+  private final JoystickButton _topButton3 = new JoystickButton(_shooterController, 6);
 
   private final DriveCamera _driveCamera = new DriveCamera();
   public final ChameleonVision _ballTracker = new ChameleonVision(CHAMELEON_CAMERA_NAME);
@@ -77,9 +90,14 @@ public class RobotContainer {
    * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    _leftShooterBumper.whenHeld(new Intake(_ballIntake));
-    _rightShooterBumper.whenHeld(new Shoot(_shooter));
+    //_leftShooterBumper.whenHeld(new Intake(_ballIntake));
+    //_rightShooterBumper.whenHeld(new Shoot(_shooter));
     _xButton.whenPressed(new FindPowerCell(_driveTrain, _ballTracker, _driverController));
+    _trigger.whenHeld(new SequentialCommandGroup(new Shoot(_shooter).withTimeout(0.5), new ParallelCommandGroup(new Shoot(_shooter), new RunHopper(_hopper), new LoadBall(_tower))));
+    _thumbButton.whenHeld(new ParallelCommandGroup(new RunArm(_ballIntake, ArmMode.DOWN), new Intake(_ballIntake)));
+    _thumbButton.whenInactive(new RunArm(_ballIntake, ArmMode.UP));
+    ((edu.wpi.first.wpilibj2.command.button.Button) _topButton0.or(_topButton1).or(_topButton2).or(_topButton3))
+        .whenHeld(new Aim(_turret, _limelight));
   }
 
   /**
