0b570ab86bef6abd86dced5213557bb96917f84c
Matt Coughlin <coughlin26@gmail.com>
2020-03-09 18:00:21 -0400

    Updated vendor libraries

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,8 +1,8 @@
 //Operates the robot motors that control the ball-shooting mechanism. 
 package frc.robot.subsystems;
 
-import static frc.robot.Constants.IntakeConstants.INTAKE_ARM_SOLENOID_PORT_OUT;
 import static frc.robot.Constants.IntakeConstants.INTAKE_ARM_SOLENOID_PORT_IN;
+import static frc.robot.Constants.IntakeConstants.INTAKE_ARM_SOLENOID_PORT_OUT;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -12,7 +12,7 @@ public class Arm extends SubsystemBase {
   // The spark that operates the motor.
   private final DoubleSolenoid arm = new DoubleSolenoid(INTAKE_ARM_SOLENOID_PORT_OUT, INTAKE_ARM_SOLENOID_PORT_IN);
 
-  public Arm(){
+  public Arm() {
   }
 
   public void armUp() {
